e323d91b41c9cdb4b063065114ec55418fd3aab8
Chris Peterson <cpeterson@mozilla.com>
2024-07-07 23:03:20 -0700

    build: avoid redefining unreachable() which is standard in C23
    In the C23 standard unreachable() is now a predefined function-like
    macro in <stddef.h>
    
    See https://android.googlesource.com/platform/bionic/+/HEAD/docs/c23.md#is-now-a-predefined-function_like-macro-in
    
    Cherry picked from upstream commit:
    
    https://gitlab.freedesktop.org/mesa/mesa/-/commit/ddf2aa3a4d305fddbe30b8e9b366887fc904d3ba

--- src/util/macros.h
+++ src/util/macros.h
@@ -72,20 +72,30 @@
  * Unreachable macro. Useful for suppressing "control reaches end of non-void
  * function" warnings.
  */
+/**
+ * Unreachable macro. Useful for suppressing "control reaches end of non-void
+ * function" warnings.
+ */
 #if defined(HAVE___BUILTIN_UNREACHABLE) || __has_builtin(__builtin_unreachable)
-#define unreachable(str)    \
+#define UNREACHABLE(str)    \
 do {                        \
+   (void)"" str; /* str must be a string literal */ \
    assert(!str);            \
    __builtin_unreachable(); \
 } while (0)
 #elif defined (_MSC_VER)
-#define unreachable(str)    \
+#define UNREACHABLE(str)    \
 do {                        \
+   (void)"" str; /* str must be a string literal */ \
    assert(!str);            \
    __assume(0);             \
 } while (0)
 #else
-#define unreachable(str) assert(!str)
+#define UNREACHABLE(str)    \
+do {                        \
+   (void)"" str; /* str must be a string literal */ \
+   assert(!str);            \
+} while (0)
 #endif
 
 /**
